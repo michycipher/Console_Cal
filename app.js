5b6a17a8ada0893c9eea293cba37eba81d5c9dc7
Michelle <michelleutomi@gmail.com>
2022-06-25 21:45:55 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let operator = prompt('Input Operator: ');
+let num1 = parseInt(prompt('First Number: '));
+let num2 = parseInt(prompt('Second Number: '));
+
+
+if(operator == '+'){
+   alert(num1 + num2) 
+}else if(operator == '-'){
+    alert(num1 - num2)
+}else if(operator == '*'){
+    alert(num1 * num2)
+}else if(operator == '/'){
+    alert(num1 / num2)
+}else if(operator == '%'){
+    alert(num1 % num2)
+}
